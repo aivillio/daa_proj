e9bfd2dde5910d3ac39d5e2fca14851d79531975
angelo <aivillio34@gmail.com>
2026-04-15 13:23:18 +0530

    .

--- uber/code.py
+++ uber/code.py
@@ -72,13 +72,13 @@ def run_test(test_num, n, values, edges, queries, expected_counts, expected_flag
 
 # Test Case 1
 run_test(
-    2,
-    4,
-    [1, 4, 6, 8],
-    [[1,2], [1,3], [2,4]],
-    [1, 2, 3, 4],
-    [0, 0, 0, 0],
-    [False, False, False, False]
+    1,
+    5,
+    [10, 7, 4, 5, 6],
+    [[1,2], [1,3], [2,4], [2,5]],
+    [1, 2, 3],
+    [2, 2, 0],
+    [True, True, False]
 )
 
 # Test Case 2 (All non-prime)
